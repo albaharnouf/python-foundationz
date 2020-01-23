3a72d7fd691ec60bdbffbf0bfff72bffd0f69397
Nouf Albahar <albahar.nouf@gmail.com>
2020-01-23 14:53:12 +0300

    done with task

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+print("Mad libs where libs get mad")
+print("Start below:")
+print
+number = input("Enter a number from 1 to 12: ")
+noun = input("Enter a noun (plural)")
+name = (input("Enter a name")).capitalize()
+scream = (input("Enter any sentence")).upper()
+action = input("Enter a verb")
+print
+print("The story goes...")
+print
+print("It was %s o'clock when I heard a knock at the door" %(number))
+print('I opened the door and there was a box full of %s with a note saying "From %s."' %(noun,name))
+print("Just as I closed the door I heard a scream %s " % (scream))
+print("I froze in place and all I could do was %s" %(action))
+
+
